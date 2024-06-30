81460fe761283a8c8d09bb22042d8cf353c39f36
hamza <hamza786504@gmail.com>
2024-06-30 14:15:45 +0500

    front home page product live attempt 2 from database

--- pages/api/product/getProducts.js
+++ pages/api/product/getProducts.js
@@ -1,5 +1,5 @@
 import mongooseConnect from '@/lib/mongoose';
-import Product from '../../db/model/productSchema';
+import Product from '../db/model/productSchema';
 
 export default async function handler(req, res) {
     await mongooseConnect();
